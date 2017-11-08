fd420eba94992bc087dc4941f34e5de058df0cea
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2017-11-07 20:11:08 -0800

    commit

---
+++ Lab3/index.php
@@ -0,0 +1,7 @@
+<!-- add stylesheets and scripts !-->
+<?php include "templates/head.php"; ?>
+<div>
+    <div class="login">Login to Continue!</div>
+</div>
+<!-- add remaining scripts -->
+<?php include "templates/footer.php"; ?>

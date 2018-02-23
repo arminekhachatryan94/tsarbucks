dba106c460a983b5286b75ae094f861fe5eadf1a
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-02-22 17:36:12 -0800

    added 2 more socket requests

--- Lab5/server.js
+++ Lab5/server.js
@@ -13,22 +13,34 @@ io.on('connection', function(socket) {
     console.log('Client connected');
 
     // send greeting 'Hello.' message to client
-    socket.emit('fromServer', {greeting: 'Hello.'});
+    socket.emit('message1FromServer', {greeting: 'Hello.'});
 
     
     // receive 'Hi there.' message from client
-    socket.on('fromClient', function(data) {
+    socket.on('message1FromClient', function(data) {
         console.log( data.greeting );
-        socket.emit('messageFromServer', {message: 'How are you?'}); // send 'How are you?' to client
+        socket.emit('message2FromServer', {message: 'How are you?'}); // send 'How are you?' to client
     });
 
-    // receive 'Great!' message from client
-    socket.on('messageFromClient', function(data) {
+    // receive 'Great! How are you?' message from client
+    socket.on('message2FromClient', function(data) {
+        console.log( data.message );
+        socket.emit('message3FromServer', {message: 'Im good!'}); // send 'Im good!' to client
+    });
+
+    // receive 'Thats awesome' message from client
+    socket.on('message3FromClient', function(data) {
+        console.log( data.message );
+        socket.emit('message4FromServer', {message: 'See you later!'}); // send 'Nice talking to you!' to client
+    });
+
+    // receive 'Goodbye!' message from client
+    socket.on('message4FromClient', function(data) {
         console.log( data.message );
     });
 
     // client disconnected
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function() {
         console.log('Client disconnected.');
     });
 });

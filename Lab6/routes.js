bb54ac44be5f023c01e4bf9d8f9ce2fb48199db0
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-03-04 18:09:55 -0800

    added ejs templates

--- Lab6/routes.js
+++ Lab6/routes.js
@@ -3,6 +3,9 @@ const exp = express();
 
 const path = require('path');
 
+exp.set('views', path.join(__dirname, 'views'));
+exp.set('view engine', 'ejs');
+
 // styles and images
 exp.use('/styles', express.static('styles'));
 exp.use(express.static('toppings'));
@@ -28,8 +31,22 @@ exp.get('/toppings', (req, res) => {
 
 exp.get('/google', (req, res) => {
     res.redirect(301, 'https://www.google.com');
- });
- 
+});
+
+// ejs
+exp.get('/special', (req, res) => {
+    res.render('special', {
+        'flavor': 'Raspberry',
+        'topping': 'Fresh Sour Cherries'
+    });
+});
+
+exp.get('/contact', (req, res) => {
+    res.render('contact', {
+        'phone': '1-800-YOG-WRLD',
+        'email': 'yogurt@yogurtworld.com'
+    });
+});
 
 // error page
 exp.get('/*', (req, res) => {

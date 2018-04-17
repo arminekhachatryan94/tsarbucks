3bf5cac4b79f819b1e51727e682277b038fa03fd
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-04-17 16:55:38 -0700

    display notes and add new note

---
+++ Lab7/notes-proj/src/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <div id="home">
+        <div><b>iNotes</b></div>
+    </div>
+
+    <note v-for="note in notes" v-text="note" :key="note.id"></note>
+    
+    <div>
+        <div>Create a New Note</div>
+        <div>
+            <input type="text" v-model="note">
+            <button type="button" v-on:click="addNote()">Post Note</button>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+import Note from '@/components/Note';
+
+
+export default {
+    name: 'Home',
+    components: {
+        Note
+    },
+  
+    data () {
+        return {
+            notes: ['Hello', 'How are you?'],
+            note: ''
+        }
+    },
+    
+    methods: {
+        addNote: function() {
+            if( this.note != '' ){
+                this.notes.push(this.note);
+                this.note = '';
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+#home {
+    background-color: pink;
+    text-align:left;
+    padding-left: 50px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    font-size: 30px;
+    color: white;
+}
+</style>
---
+++ Lab7/notes-proj/src/components/Note.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <div class="note-style" v-bind="note"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Note',
+  data () {
+    return {
+      note: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.note-style {
+  padding-top: 50px;
+}
+</style>
